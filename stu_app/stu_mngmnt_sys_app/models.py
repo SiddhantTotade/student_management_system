e5047c928830a541678abee783e8b51f6f2b4835
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-09-19 22:53:07 +0530

    some changes in models.py

--- stu_app/stu_mngmnt_sys_app/models.py
+++ stu_app/stu_mngmnt_sys_app/models.py
@@ -1,14 +1,19 @@
-from importlib.util import module_for_loader
-from turtle import update
-from venv import create
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
+class CustomUser(AbstractUser):
+    user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
+    user_type = models.CharField(
+        default=1, choices=user_type_data, max_length=10)
+
 # Admin model
+
+
 class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
@@ -52,6 +57,7 @@ class Subjects(models.Model):
 # Student model
 class Students(models.Model):
     id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
@@ -59,6 +65,8 @@ class Students(models.Model):
     profile_pic = models.FileField()
     address = models.TextField()
     course_id = models.ForeignKey(Courses, on_delete=models.DO_NOTHING)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
--- stu_app/student_management_system/settings.py
+++ stu_app/student_management_system/settings.py
@@ -135,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "stu_mngmnt_sys_app.CustomUser"
+ 
