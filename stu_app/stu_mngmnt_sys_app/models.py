b3e90e52073a71f13ac3290f995b7fee72579f89
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-09-20 13:30:00 +0530

    chnages in html

--- stu_app/stu_mngmnt_sys_app/models.py
+++ stu_app/stu_mngmnt_sys_app/models.py
@@ -2,18 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+# CustomUser model
+
 
 class CustomUser(AbstractUser):
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(
         default=1, choices=user_type_data, max_length=10)
 
+
 # Admin model
-
-
 class AdminHOD(models.Model):
     id = models.AutoField(primary_key=True)
-    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    admin = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
--- stu_app/student_management_system/settings.py
+++ stu_app/student_management_system/settings.py
@@ -137,4 +137,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "stu_mngmnt_sys_app.CustomUser"
- 
