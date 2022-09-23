46a927abaaa5113df4c5b45618391932193a3ee7
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-09-23 15:13:14 +0530

    changes in models.py file

--- stu_app/stu_mngmnt_sys_app/models.py
+++ stu_app/stu_mngmnt_sys_app/models.py
@@ -4,9 +4,8 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 # Create your models here.
 
+
 # CustomUser model
-
-
 class CustomUser(AbstractUser):
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(
