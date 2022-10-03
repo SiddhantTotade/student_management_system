53d5c45247c578480901465adf896c9f0b1bf8f2
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-10-03 18:53:37 +0530

    home page of student and staff done

--- stu_app/stu_mngmnt_sys_app/hodViews.py
+++ stu_app/stu_mngmnt_sys_app/hodViews.py
@@ -11,7 +11,65 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Redndering home page
 def admin_home(request):
-    return render(request, 'hod_template/home_content.html')
+    student_count = Students.objects.all().count()
+    subject_count = Subjects.objects.all().count()
+    course_count = Courses.objects.all().count()
+    staff_count = Staffs.objects.all().count()
+
+    course_all = Courses.objects.all()
+    course_name_list = []
+    subject_count_list = []
+    student_count_list_in_course = []
+
+    for course in course_all:
+        student = Students.objects.filter(course_id=course.id).count()
+        subject = Subjects.objects.filter(course_id=course.id).count()
+        course_name_list.append(course.course_name)
+        subject_count_list.append(subject)
+        student_count_list_in_course.append(student)
+
+    # Total student in each subject
+    subject_all = Subjects.objects.all()
+    subject_list = []
+    student_count_list_in_subject = []
+    for subject in subject_all:
+        course = Courses.objects.get(id=subject.course_id.id)
+        student_count = Students.objects.filter(course_id=course.id).count()
+        subject_list.append(subject.subject_name)
+        student_count_list_in_subject.append(student_count)
+
+    # Staff leave vs attendance
+    staffs_all = Staffs.objects.all()
+    attendance_present_list_staff = []
+    attendance_absent_list_staff = []
+    staff_name_list = []
+    for staff in staffs_all:
+        subject_id = Subjects.objects.filter(staff_id=staff.admin.id)
+        attendance = Attendance.objects.filter(
+            subject_id__in=subject_id).count()
+        leave = LeaveReportStaff.objects.filter(
+            staff_id=staff.id, leave_status=1).count()
+        attendance_present_list_staff.append(attendance)
+        attendance_absent_list_staff.append(leave)
+        staff_name_list.append(staff.admin.username)
+
+    # Student leave vs attendance
+    students_all = Students.objects.all()
+    attendance_present_list_student = []
+    attendance_absent_list_student = []
+    student_name_list = []
+    for student in students_all:
+        attendance = AttendanceReport.objects.filter(
+            student_id=student.id,status=True).count()
+        absent = AttendanceReport.objects.filter(
+            student_id=student.id,status=False).count()
+        leave = LeaveReportStudent.objects.filter(
+            student_id=student.id, leave_status=1).count()
+        attendance_present_list_student.append(attendance)
+        attendance_absent_list_student.append(leave+absent)
+        student_name_list.append(student.admin.username)
+
+    return render(request, 'hod_template/home_content.html', {'student_count': student_count, 'subject_count': subject_count, 'course_count': course_count, 'staff_count': staff_count, 'course_name_list': course_name_list, 'subject_count_list': subject_count_list, 'student_count_list_in_course': student_count_list_in_course, 'student_count_list_in_subject': student_count_list_in_subject, 'subject_list': subject_list, 'staff_name_list': staff_name_list, 'attendance_present_list_staff': attendance_present_list_staff, 'attendance_absent_list_staff': attendance_absent_list_staff, 'attendance_absent_list_student': attendance_absent_list_student, 'student_name_list': student_name_list, 'attendance_present_list_student': attendance_present_list_student})
 
 
 # Rendering add_staff page
