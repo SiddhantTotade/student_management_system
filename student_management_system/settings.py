4148306440507c2cc7e1a9d953d4067054bc7663
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-10-07 15:25:54 +0530

    psycopg2 installed and django_on_heroku installed

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import django_on_heroku
 from stu_mngmnt_sys_app import password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,16 +85,27 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         # 'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': 'student_management_system',
+#         # 'USER': 'student_management_system',
+#         # 'PASSWORD': 'student_management_password',
+#         # 'HOST': 'localhost',
+#         # 'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'student_management_system',
-        # 'USER': 'student_management_system',
-        # 'PASSWORD': 'student_management_password',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'drn358csfd1a',
+        'USER':'mbrekkgrezoaad',
+        'PASSWORD':'abed6863680debd4976bf07493832abaa91db28fa47466710db2d617688fb9ea',
+        'HOST':'ec2-3-214-2-141.compute-1.amazonaws.com',
+        'PORT':'5432',
     }
 }
 
